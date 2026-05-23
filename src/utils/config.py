841dbcb14abce9017bb383fa6f34737b07fe12ff
bobby <bobby.harris@gmail.com>
2026-05-23 14:27:02 +0200

    working on bai-ubuntu

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,3 +1,4 @@
 G = 2941.18330364  # R_s^3 M_s^-1 day^−2
-MODEL_PATH = '/users/malharris/lpp_search/model_TESS.pth'
+MODEL_PATH = '/home/bobby/dev/python/lpp_search/model_TESS.pth'
 MDWARF_CATALOG = '/users/malharris/lpp_search/data/final_mdwarf_params.csv'
+
